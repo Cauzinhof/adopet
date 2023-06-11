4fc278a97066af06e642ac9029250315a71e8a90
Carlos Fernandes <carlos.f.junior@outlook.com>
2023-06-10 20:15:40 -0400

    DB de pets atualizada e de abrigos criada

--- apps/adopets/models.py
+++ apps/adopets/models.py
@@ -11,12 +11,15 @@ class Pet(models.Model):
         ('G', 'Porte grande')
     ]
 
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True, default='')
+    abrigo = models.ForeignKey(to='Abrigo', on_delete=models.CASCADE, null=True)
     nome = models.CharField(max_length=20)
     idade = models.CharField(max_length=10)
     porte = models.CharField(max_length=20, choices=PORTE)
     caracteristicas = models.CharField(max_length=30)
     cidade = models.CharField(max_length=30)
     estado = models.CharField(max_length=2)
+    adotado = models.BooleanField(default=False)
 
 class Tutor(models.Model):
     foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True, default='')
@@ -26,3 +29,9 @@ class Tutor(models.Model):
     cidade = models.CharField(max_length=20)
     estado = models.CharField(max_length=2, default='MS', blank=False)
     sobre = models.TextField()
+
+class Abrigo(models.Model):
+    nome = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    cidade = models.CharField(max_length=20)
+    estado = models.CharField(max_length=2, default='MS', blank=False)
--- apps/adopets/serializer.py
+++ apps/adopets/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Tutor, Pet
+from .models import Tutor, Pet, Abrigo
 from .validators import *
 
 class TutorSerializer(serializers.ModelSerializer):
@@ -23,4 +23,5 @@ class TutorSerializer(serializers.ModelSerializer):
 class PetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pet
-        fields = '__all__'
+        exclude = ['foto']
+
