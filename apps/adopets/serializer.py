7b2fd4d045f0232f0c63a96c4705da5823f8e067
Carlos Fernandes <carlos.f.junior@outlook.com>
2023-06-10 21:10:00 -0400

    Modificação para o PATCH funcionar

--- apps/adopets/serializer.py
+++ apps/adopets/serializer.py
@@ -10,6 +10,8 @@ class TutorSerializer(serializers.ModelSerializer):
         fields = ['nome', 'email', 'telefone', 'cidade', 'estado', 'sobre']
 
     def validate(self, attrs):
+        if self.partial:
+            return attrs
         if not chars_valido(attrs['nome']):
             raise serializers.ValidationError({'nome':"O nome deve conter apenas letras"})
         if not telefone_valido(attrs['telefone']):
@@ -26,6 +28,8 @@ class PetSerializer(serializers.ModelSerializer):
         exclude = ['foto']
 
     def validate(self, attrs):
+        if self.partial:
+            return attrs
         if not chars_valido(attrs['nome']):
             raise serializers.ValidationError({'nome':"O nome deve conter apenas letras"})
         if not chars_valido(attrs['cidade']):
@@ -40,6 +44,8 @@ class AbrigoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate(self, attrs):
+        if self.partial:
+            return attrs
         if not chars_valido(attrs['nome']):
             raise serializers.ValidationError({'nome':"O nome deve conter apenas letras"})
         if not chars_valido(attrs['cidade']):
